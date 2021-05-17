379567a8cd7d31953a80da3746c21877e8dd5b13
DenisGridin <denisgridin6@gmail.com>
2021-05-17 17:55:23 +0300

    fix header

--- components/Headers/HeaderApp.vue
+++ components/Headers/HeaderApp.vue
@@ -11,6 +11,12 @@
             <span>Транслировать</span>
           </div>
         </vs-button>
+        <vs-button circle>
+          <div class="export-button" @click="exportInPng">
+            <i class="bx bx-export"></i>
+            <span>Экспорт в PNG</span>
+          </div>
+        </vs-button>
       </div>
       <HeaderButtons />
     </template>
@@ -29,6 +35,7 @@ export default class HeaderApp extends Vue {
 
   playPresentation () {
     const path = this.$route.fullPath
+    PresentationModule.SET_ACTIVE_ELEMENT_ID_AND_TYPE({ id: null, type: null })
     this.$router.push({ path: path.replace('constructor', 'broadcast') })
   }
 }
